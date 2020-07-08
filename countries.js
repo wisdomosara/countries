cd006988a9fd48d0b2d620394dd4cc23f9466ce7
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-08 03:01:25 +0100

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,25 @@
+const searchbox = document.querySelector(".search-input");
+const darkmode = document.querySelector(".darkmode");
+const selectbox = document.querySelector("select");
+const container = document.querySelector("main");
+
+    fetch('https://restcountries.eu/rest/v2/all')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(element => {  
+        container.innerHTML += `
+            <div class="country-div">
+                <div class="img-div">
+                    <img src=${element.flag} width="100%" height="100%" >
+                </div>
+                <div class="country-example">
+                    <h4>${element.name}</h4>
+                    <p><strong>Population:</strong> ${element.population}</p>
+                    <p><strong>Region:</strong> ${element.region}</p>
+                    <p><strong>Capital:</strong> ${element.capital}</p>
+                </div>
+            </div>
+      `
+    });
+    
+  })
